117e0f0c69db992ba0570dacf1cc605e529f1bb2
Joseph Specht <jspecht3@illinois.edu>
2024-11-08 13:50:29 -0600

    saving

--- npre449/hw8/hw8.py
+++ npre449/hw8/hw8.py
@@ -39,14 +39,58 @@ def q2():
 
 # question 3
 def q3():
-    Eloss = 0.05 * 1e3 * (103 - 77)
+    Eloss = 0.05 * 1043 * (103 - 77)
     qpp = Eloss / (np.pi * 0.15 * 5)
     Ts = qpp / 6
 
+    re = 4 * 0.05 / 2.068e-5 / np.pi / 0.15
+    qpp = 77 / ((1/11.84) + (1/6))
+    tw = qpp / 6
+
     print(f"----- Q3 -----\n" +
           f"Edot_loss: {Eloss} W\n" +
-          f"q\'\': {qpp} W/m^2\n" +
-          f"Ts: {Ts} C")
+          f"Re: {re}\n" +
+          f"q\" {qpp} w/m^2\n" +
+          f"Tw {tw} K")
 
+# question 4
+def q4():
+    h = 3.66 * 0.138 / 50e-3
+    
+    exp = h * np.pi * 50e-3 * 25 / 2131 / 0.5
+    tl = 150-(150-20)*np.exp(-exp)
+    q = 0.5 * 2131 * (tl - 20)
 
-q3()
+    print(f"----- Q4 -----\n" +
+          f"h: {h} W/m^2*K\n" +
+          f"T(L): {tl} *C\n" +
+          f"q: {q} W")
+
+# question 5
+def q5():
+    cp = 4182
+    mdot = 2
+    d = 40e-3
+    h = 6918.15
+    perm_h = d * np.pi
+    mu = 8.9e-4
+    
+    A = d**2 / 4 * np.pi
+    G = mdot / A
+
+    f = 0.0174
+    
+    logger = np.log((25 - 100) / (75 - 100))
+    L = cp * mdot / h / perm_h * logger
+
+    re = 116415
+    pr = mu * cp / 0.6
+
+    dp = f * G**2 * L / 2 / 1000 / d
+    
+    print(f"----- Q5 -----\n" +
+          f"L for 75*C: {L}\n" +
+          f"Pr: {pr}\n" +
+          f"dP: {dp}")
+
+q5()
